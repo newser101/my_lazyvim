5ae408e3186a18556079f033d648b0162cc20f2d
newser101 <nepumuk101@gmail.com>
2023-11-21 19:42:41 +0100

    add exit keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+-- Press XX fast to exit insert moder
+keymap("i", "jk", "<ESC>", opts)
