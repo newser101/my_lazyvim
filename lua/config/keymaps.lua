82e0974b8df106f254fda3744b847a0063bfdd60
newser101 <nepumuk101@gmail.com>
2023-11-22 15:10:49 +0100

    add quit and save keybinding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,33 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
+-- function to add desc to opts
+local function new_opts(text)
+  -- local newtable = { noremap = true, silent = true }
+  local newtable = opts
+  newtable.desc = text
+  return newtable
+end
+
 -- Press XX fast to exit insert moder
 keymap("i", "jk", "<ESC>", opts)
+
+-- replace keymaps
+
+-- save file
+-- try to clear global keymaps
+keymap({ "i", "x", "n", "s" }, "<C-s>", "<nop>", {}) -- delete orig save keymap
+keymap("n", "<leader>w", "<CMD>w<CR><ESC>", new_opts("new Save file")) -- save file
+-- close buffer
+keymap("n", "<leader>q", "<CMD>q<CR>", new_opts("quit")) -- quit file
