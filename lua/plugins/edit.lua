90c456d343b4805910ab9ecb92cd96362ce37b06
newser101 <nepumuk101@gmail.com>
2023-11-26 17:01:06 +0100

    edit friendly snippets

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -160,6 +160,7 @@ return {
   {
     "rafamadriz/friendly-snippets",
     config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
       -- require("luasnip.loaders.from_lua").load({ paths = "/home/cawl/.config/nvim/lua/snippets" })
       require("luasnip.loaders.from_lua").lazy_load({ paths = "/home/cawl/.config/nvim/lua/snippets" })
     end,
