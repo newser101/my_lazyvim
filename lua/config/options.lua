263af51a5fbaf51e94da708ac49ffe64fa79f3ff
newser101 <nepumuk101@gmail.com>
2023-12-08 19:50:28 +0100

    add german spelling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+
 -- show CR symbol at end of the line
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
+
+-- add german to spellcheck
+opt.spelllang = { "en", "de" }
