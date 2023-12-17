7371df9a370788e39f894772acc0afccb38379e9
newser101 <nepumuk101@gmail.com>
2023-12-17 13:14:14 +0100

    comment lsp settings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,90 +1,90 @@
 return {
   -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        -- "query",
-        "regex",
-        -- "tsx",
-        -- "typescript",
-        "vim",
-        -- "yaml",
-      },
-    },
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = {
+  --     ensure_installed = {
+  --       "bash",
+  --       "html",
+  --       "javascript",
+  --       "json",
+  --       "lua",
+  --       "markdown",
+  --       "markdown_inline",
+  --       "python",
+  --       -- "query",
+  --       "regex",
+  --       -- "tsx",
+  --       -- "typescript",
+  --       "vim",
+  --       -- "yaml",
+  --     },
+  --   },
+  -- },
   -- add more treesitter parsers END
 
   -- add formatter to Conform
-  {
-    "stevearc/conform.nvim",
-    opts = function()
-      local opts = {
-        ---@type table<string, conform.FormatterUnit[]>
-        formatters_by_ft = {
-          lua = { "stylua" },
-          sh = { "shfmt" },
-          markdown = { "mdformat" },
-          python = { "black" },
-          -- fish = { "fish_indent" },
-        },
-      }
-      return opts
-    end,
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = function()
+  --     local opts = {
+  --       ---@type table<string, conform.FormatterUnit[]>
+  --       formatters_by_ft = {
+  --         lua = { "stylua" },
+  --         sh = { "shfmt" },
+  --         markdown = { "mdformat" },
+  --         python = { "black" },
+  --         -- fish = { "fish_indent" },
+  --       },
+  --     }
+  --     return opts
+  --   end,
+  -- },
   -- add formatter to Conform END
 
   -- add Linter
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-      linters_by_ft = {
-        -- fish = { "fish" },
-        markdown = { "markdownlint" },
-        -- python = { "flake8" },
-        python = { "ruff" },
-        -- Use the "*" filetype to run linters on all filetypes.
-        -- ['*'] = { 'global linter' },
-        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-        -- ['_'] = { 'fallback linter' },
-      },
-    },
-  },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   opts = {
+  --     events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  --     linters_by_ft = {
+  --       -- fish = { "fish" },
+  --       markdown = { "markdownlint" },
+  --       -- python = { "flake8" },
+  --       python = { "ruff" },
+  --       -- Use the "*" filetype to run linters on all filetypes.
+  --       -- ['*'] = { 'global linter' },
+  --       -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+  --       -- ['_'] = { 'fallback linter' },
+  --     },
+  --   },
+  -- },
   -- add Linter END
 
   -- add lsp to Mason
-  {
-
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- formatters:
-        "stylua",
-        "shfmt",
-        "mdformat",
-        "black",
-
-        -- linter:
-        "markdownlint",
-        "flake8",
-        "ruff",
-
-        -- lsp:
-        "pyright",
-        "marksman",
-        "lua-language-server",
-        "json-lsp",
-      },
-    },
-  },
+  -- {
+  --
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       -- formatters:
+  --       "stylua",
+  --       "shfmt",
+  --       "mdformat",
+  --       "black",
+  --
+  --       -- linter:
+  --       "markdownlint",
+  --       "flake8",
+  --       "ruff",
+  --
+  --       -- lsp:
+  --       "pyright",
+  --       "marksman",
+  --       "lua-language-server",
+  --       "json-lsp",
+  --     },
+  --   },
+  -- },
   -- add lsp to Mason END
 }
