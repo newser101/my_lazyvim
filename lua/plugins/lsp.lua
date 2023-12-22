71d0b824390a01a9ec368c9c4bcfe23cb2fa1ebc
newser101 <nepumuk101@gmail.com>
2023-12-22 21:02:55 +0100

    edit for java and python dap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,45 +11,34 @@ return {
           vim.list_extend(opts.ensure_installed, { "java-test", "java-debug-adapter" })
         end,
       },
+      {
+        "mfussenegger/nvim-dap-python",
+        keys = {
+          {
+            "<leader>dPt",
+            function()
+              require("dap-python").test_method()
+            end,
+            desc = "Debug Method",
+            ft = "python",
+          },
+          {
+            "<leader>dPc",
+            function()
+              require("dap-python").test_class()
+            end,
+            desc = "Debug Class",
+            ft = "python",
+          },
+        },
+        config = function()
+          local path = require("mason-registry").get_package("debugpy"):get_install_path()
+          require("dap-python").setup(path .. "/venv/bin/python")
+        end,
+      },
     },
   },
   -- add for java END
-
-  -- add none-ls
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   opts = function(_, opts)
-  --     local nls = require("null-ls")
-  --     opts.sources = opts.sources or {}
-  --     table.insert(opts.sources, nls.builtins..black)
-  --     table.insert(opts.sources, nls.builtins.formatting.prettier)
-  --   end,
-  -- },
-  -- add none-ls END
-
-  -- add more treesitter parsers
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = {
-  --     ensure_installed = {
-  --       "bash",
-  --       "html",
-  --       "javascript",
-  --       "json",
-  --       "lua",
-  --       "markdown",
-  --       "markdown_inline",
-  --       "python",
-  --       -- "query",
-  --       "regex",
-  --       -- "tsx",
-  --       -- "typescript",
-  --       "vim",
-  --       -- "yaml",
-  --     },
-  --   },
-  -- },
-  -- add more treesitter parsers END
 
   -- add formatter to Conform
   {
@@ -68,48 +57,45 @@ return {
   -- add formatter to Conform END
 
   -- add Linter
-  -- {
-  --   "mfussenegger/nvim-lint",
-  --   opts = {
-  --     events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-  --     linters_by_ft = {
-  --       --       -- fish = { "fish" },
-  --       --       markdown = { "markdownlint" },
-  --       python = { "flake8" },
-  --       --       python = { "ruff" },
-  --       --       -- Use the "*" filetype to run linters on all filetypes.
-  --       --       -- ['*'] = { 'global linter' },
-  --       --       -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-  --       --       -- ['_'] = { 'fallback linter' },
-  --     },
-  --   },
-  -- },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        --       -- fish = { "fish" },
+        markdown = { "markdownlint" },
+        -- python = { "flake8" },
+        --       python = { "ruff" },
+        --       -- Use the "*" filetype to run linters on all filetypes.
+        --       -- ['*'] = { 'global linter' },
+        --       -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        --       -- ['_'] = { 'fallback linter' },
+      },
+    },
+  },
   -- add Linter END
 
-  -- add lsp to Mason
-  -- {
-  --
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       -- formatters:
-  --       "stylua",
-  --       "shfmt",
-  --       "mdformat",
-  --       "black",
-  --
-  --       -- linter:
-  --       "markdownlint",
-  --       "flake8",
-  --       "ruff",
-  --
-  --       -- lsp:
-  --       "pyright",
-  --       "marksman",
-  --       "lua-language-server",
-  --       "json-lsp",
-  --     },
-  --   },
-  -- },
-  -- add lsp to Mason END
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- "bash",
+        "html",
+        -- "javascript",
+        -- "json",
+        -- "lua",
+        -- "markdown",
+        -- "markdown_inline",
+        -- "python",
+        -- "query",
+        -- "regex",
+        -- "tsx",
+        -- "typescript",
+        -- "vim",
+        -- "yaml",
+      },
+    },
+  },
+  -- add more treesitter parsers END
 }
