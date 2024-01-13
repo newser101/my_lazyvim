3ec436cc16d014ff3c146f29f3ed40cebbbadd88
newser101 <nepumuk101@gmail.com>
2024-01-13 22:19:35 +0100

    add snippets

--- lua/snippets/sh.lua
+++ lua/snippets/sh.lua
@@ -1,8 +1,23 @@
 local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
 
 ls.add_snippets("sh", {
 
-  s({ trig = "shebang" }, {
-    t({ "#!/bin/bash\n" }),
+  s("trigger", {
+    i(1),
+    t("text"),
+    i(2),
+    t("text again"),
+    i(3),
+  }),
+
+  s("triggernew_line", {
+    t({ "first line", "next line" }),
+  }),
+
+  s("snippet", {
+    t('s("")'),
   }),
 })
