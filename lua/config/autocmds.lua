4405fd17e99c35c86996075463ba787a48b768a5
newser101 <nepumuk101@gmail.com>
2024-01-20 14:19:18 +0100

    add dondt comment next line

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- remove auto comment line after o from comment line
+vim.api.nvim_create_autocmd("FileType", {
+  command = "set formatoptions-=cro",
+})
