070e15cc3f7096fb6d45f6262d9d3a2e72ae5783
newser101 <nepumuk101@gmail.com>
2024-01-20 14:20:09 +0100

    edit snippets

--- lua/snippets/lua.lua
+++ lua/snippets/lua.lua
@@ -9,21 +9,13 @@ return {
     t("lua snippet loaded"),
   }),
 
+  -- snippet snippet
   s("snippet", {
-    t({ 's("' }),
-    i(1),
-    t({ '",{', "" }),
+    t('s("'),
+    i(1, "trigger"),
+    t({ '", {', "" }),
     i(0),
     t({ "", "})," }),
-  }),
-
-  s("trigger", {
-    t({ "After expanding, the cursor is here ->" }),
-    i(1),
-    t({ "", "After jumping forward once, cursor is here ->" }),
-    i(2),
-    t({ "", "After jumping once more, the snippet is exited there ->" }),
-    i(0),
   }),
 }
 
--- lua/snippets/sh.lua
+++ lua/snippets/sh.lua
@@ -3,12 +3,35 @@ local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
 
+-- ls.add_snippets({ trig = "sh", priority = 2000 }, {
 ls.add_snippets("sh", {
+
   s("trig", {
     t("sh snippets loaded"),
   }),
 
-  s("shebang", {
+  -- s({ trig = "shebang", priority = 2000 }, {
+  --   t({ "#!/bin/bash", "" }),
+  -- }),
+
+  -- s("shebang", {
+  --   t({ "#!/bin/bash", "" }),
+  -- }),
+
+  s({ trig = "shebang", priority = 1500 }, {
     t({ "#!/bin/bash", "" }),
+  }),
+
+  -- testing snippets
+  -- insert_node
+  s("insertnode", {
+    t({ "First line " }),
+    i(1),
+    t({ " " }),
+    i(2, "editable"),
+    t(" :: "),
+    i(3, "also"),
+    t({ "", "newline", "" }),
+    i(0, "End"),
   }),
 })
