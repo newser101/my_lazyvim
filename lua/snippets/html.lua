cf7fe484bc8cccefae954bc83d83f8fc0242c752
newser101 <nepumuk101@gmail.com>
2024-01-21 20:29:36 +0100

    edit snippets

--- lua/snippets/html.lua
+++ lua/snippets/html.lua
@@ -2,4 +2,31 @@ return {
   s("trig", {
     t("html snippet loaded!!"),
   }),
+
+  s({ trig = "doctype", priority = 1500 }, {
+    t({ "<!doctype html>", "" }),
+  }),
+
+  --     <link href="" rel="stylesheet">
+  --   </head>
+  --   <body>
+  --
+  --   </body>
+  -- </html>
+  s({ trig = "html5", priority = 1500, desc = "core html body to start", name = "core html" }, {
+    t({ "<!doctype html>", '<html lang="' }),
+    i(1, "en"),
+    t({ '">', "<head>", "<title>" }),
+    i(2, "title"),
+    t({
+      "</title>",
+      '<meta charset="UTF-8">',
+      '<meta name="viewport" content="width=device-width, initial-scale=1">',
+      '<link href="',
+    }),
+    i(3, "css/style.css"),
+    t({ '" rel="stylesheet">', "</head>", "<body>", "" }),
+    i(0),
+    t({ "", "</body>", "</html>", "" }),
+  }),
 }
--- lua/snippets/sh.lua
+++ lua/snippets/sh.lua
@@ -1,21 +1,13 @@
--- local ls = require("luasnip")
--- local s = ls.snippet
+local ls = require("luasnip")
+local s = ls.snippet
 -- local i = ls.insert_node
--- local t = ls.text_node
+local t = ls.text_node
 -- local f = ls.function_node
 -- local fmt = require("luasnip.extras.fmt").fmt
 
-local function fn(
-  args, -- text from i(2) in this example i.e. { { "456" } }
-  parent, -- parent snippet or parent node
-  user_args -- user_args from opts.user_args
-)
-  return "[" .. args[1][1] .. user_args .. "]"
-end
-
 -- ls.add_snippets({ trig = "sh", priority = 2000 }, {
--- ls.add_snippets("sh", {
-return {
+ls.add_snippets("sh", {
+  -- return {
   s("trig", {
     t("sh snippets loaded"),
   }),
@@ -24,21 +16,5 @@ return {
     t({ "#!/bin/bash", "" }),
   }),
 
-  -- testing snippets
-  -- test function note
-
-  s("func12", {
-    t({ "Test:", "" }),
-    i(1),
-    t({ "", "" }),
-    i(2),
-    t({ "", "Function output:", "" }),
-    f(
-      fn, -- callback (args, parent, user_args) -> string
-      { 2 }, -- node indice(s) whose text is passed to fn, i.e. i(2)
-      { user_args = { "user_args_value" } } -- opts
-    ),
-    i(0),
-  }),
-}
--- })
+  -- }
+})
